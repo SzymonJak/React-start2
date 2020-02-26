34bf2b8aef5b9d1207da75a9410dd04b38d0bfd9
Szymon Jakubiak <jakubiak.sz@gmail.com>
2020-02-26 19:08:37 +0100

    Add ESlint and Husky

---
+++ src/components/Column/Column.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styles from './Column.scss';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator';
+import Icon from '../Icon/Icon';
+
+class Column extends React.Component {
+    state = {
+      cards: this.props.cards || [],
+    }
+
+    static propTypes = {
+      title: PropTypes.node,
+      icon: PropTypes.string,
+      cards: PropTypes.array,
+    }
+
+    addCard(title){
+      this.setState(state => (
+        {
+          cards: [
+            ...state.cards,
+            {
+              key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+              title,
+            },
+          ],
+        }
+      ));
+    }
+
+    render () {
+      return (
+        <section className={styles.component}>
+          <h3 className={styles.title}>
+            <span className={styles.icon}>
+              <Icon name={this.props.icon}></Icon>
+            </span>
+            {this.props.title}
+          </h3>
+          <div>
+            {this.state.cards.map(({key, ...cardProps}) => (
+              <Card key={key} {...cardProps} />
+            ))}
+          </div>
+          <div>
+            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+          </div>
+        </section>
+      );
+    }
+}
+
+export default Column;
